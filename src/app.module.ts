948aa5f790853b5b809a2e044fa2438e62c88290
Manuel Martínez <manuel.mtzv816@gmail.com>
2024-07-11 22:24:41 -0600

    feat: implement basics of mikro-orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { PostgreSqlDriver } from '@mikro-orm/postgresql';
 
 @Module({
-  imports: [],
+  imports: [
+    MikroOrmModule.forRootAsync({
+      useFactory: () => ({
+        entities: ['./dist/entities'],
+        entitiesTs: ['./src/entities'],
+        dbName: 'fishkeepers-hub-db',
+        password: 'password',
+        user: 'postgres',
+        driver: PostgreSqlDriver,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
