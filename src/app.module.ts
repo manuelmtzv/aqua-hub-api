956b8a0359be87d6ce55dceadf026dce247a244c
Manuel Martínez <manuel.mtzv816@gmail.com>
2024-07-14 11:54:58 -0600

    feat: configure config, auth and health module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
-import mikroOrmConfig from '~/mikro-orm.config';
+import { ConfigModule } from '@nestjs/config';
 
+import mikroOrmConfig from '~/mikro-orm.config';
+import { AuthModule, HealthModule } from '@/modules';
 @Module({
   imports: [
     MikroOrmModule.forRootAsync({
@@ -12,8 +12,14 @@ import mikroOrmConfig from '~/mikro-orm.config';
         autoLoadEntities: true,
       }),
     }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    AuthModule,
+    HealthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
