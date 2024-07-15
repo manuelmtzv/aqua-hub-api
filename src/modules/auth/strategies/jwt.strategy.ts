8bebed759a0b9b67e9dae7069c4eba64c0916054
Manuel Martínez <manuel.mtzv816@gmail.com>
2024-07-14 21:38:02 -0600

    chore: refactor user service import

--- src/modules/auth/strategies/jwt.strategy.ts
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -3,7 +3,7 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtPayload } from '../types/JwtPayload';
 import { ConfigService } from '@nestjs/config';
-import { UserService } from '../../user';
+import { UserService } from '@/modules/user/user.service';
 import { User } from '~/src/entities';
 
 @Injectable()
