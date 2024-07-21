100575434e87d272531719dc10a417381512bdf2
Manuel Martínez <manuel.mtzv816@gmail.com>
2024-07-21 16:20:09 -0600

    chore: update app port configuration in .env.example and main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { validationConfig } from './configs';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = app.get(ConfigService);
 
   app.enableCors();
   app.setGlobalPrefix('api');
 
   validationConfig(app);
 
-  await app.listen(3000);
+  await app.listen(config.get('PORT') || 3000);
 }
 bootstrap();
