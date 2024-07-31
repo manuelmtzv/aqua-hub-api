b89fc4377ff6d5d45601128360f59bb8662b1213
Your Name <manuel.mtzv816@gmail.com>
2024-07-31 14:29:56 -0600

    feat: implement post-comment controller with create post comment endpoint

---
+++ src/modules/post/controllers/post-comment.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Param, Post } from '@nestjs/common';
+import { GetUser } from '@/modules/auth/decorators/getUser.decorator';
+import { CommentService } from '@/modules/comment/comment.service';
+import { CreateCommentDto } from '@/modules/comment/dtos';
+
+@Controller('posts/:postId/comments')
+export class PostCommentController {
+  constructor(private readonly commentService: CommentService) {}
+
+  @Post()
+  async createPostComment(
+    @Param('postId') postId: string,
+    @GetUser('id') userId: string,
+    @Body() createComment: CreateCommentDto,
+  ) {
+    return this.commentService.create(userId, 'post', postId, createComment);
+  }
+}
