d0e9e7e5fd49e010a062e1ba9e0a6eacf7a7d13c
Your Name <manuel.mtzv816@gmail.com>
2024-07-31 14:41:09 -0600

    fix: remove reaction controller

--- src/modules/reaction/reaction.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { ReactionService } from './reaction.service';
-
-@Controller('reactions')
-export class ReactionController {
-  constructor(private readonly reactionService: ReactionService) {}
-}
--- src/modules/reaction/reaction.module.ts
+++ src/modules/reaction/reaction.module.ts
@@ -1,12 +1,10 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
 import { Reaction } from '~/src/entities';
-import { ReactionController } from './reaction.controller';
 import { ReactionService } from './reaction.service';
 
 @Module({
   imports: [MikroOrmModule.forFeature([Reaction])],
-  controllers: [ReactionController],
   providers: [ReactionService],
   exports: [ReactionService],
 })
