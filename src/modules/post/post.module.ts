c5f5f14d26e8f94c7bbdcaad6b50a8f0ed4b7e8b
Manuel Martínez <manuel.mtzv816@gmail.com>
2024-07-31 22:14:24 -0600

    feat: create post reaction controller and create endpoint

--- src/modules/post/post.module.ts
+++ src/modules/post/post.module.ts
@@ -3,10 +3,13 @@ import { Module } from '@nestjs/common';
 import { Post } from '~/src/entities';
 import { PostController } from './post.controller';
 import { PostService } from './post.service';
+import { PostCommentController, PostReactionController } from './controllers';
+import { CommentModule } from '@/modules/comment/comment.module';
+import { ReactionModule } from '@/modules/reaction/reaction.module';
 
 @Module({
-  imports: [MikroOrmModule.forFeature([Post])],
-  controllers: [PostController],
+  imports: [MikroOrmModule.forFeature([Post]), CommentModule, ReactionModule],
+  controllers: [PostController, PostCommentController, PostReactionController],
   providers: [PostService],
   exports: [PostService],
 })
