20697ac71b6868052c1e3780bf3f8e06123ff06f
Your Name <manuel.mtzv816@gmail.com>
2024-08-08 14:47:15 -0600

    feat: define user abilities in depth

--- src/seeders/role.seeder.ts
+++ src/seeders/role.seeder.ts
@@ -11,7 +11,70 @@ export class RoleSeeder extends Seeder {
 
     em.create(Role, {
       name: 'user',
-      permissions: [{ action: 'read', subject: 'all' }],
+      permissions: [
+        { action: 'read', subject: 'all' },
+        // Own permissions
+        {
+          action: 'update',
+          subject: 'User',
+          condition: { id: '${user.id}' },
+        },
+        // Post permissions
+        {
+          action: 'create',
+          subject: 'Post',
+        },
+        {
+          action: 'update',
+          subject: 'Post',
+          condition: { authorId: '${user.id}' },
+        },
+        {
+          action: 'delete',
+          subject: 'Post',
+          condition: { authorId: '${user.id}' },
+        },
+        // Comment permissions
+        {
+          action: 'create',
+          subject: 'Comment',
+        },
+        {
+          action: 'update',
+          subject: 'Comment',
+          condition: { authorId: '${user.id}' },
+        },
+        {
+          action: 'delete',
+          subject: 'Comment',
+          condition: { authorId: '${user.id}' },
+        },
+        // Comment permissions
+        {
+          action: 'create',
+          subject: 'Comment',
+        },
+        {
+          action: 'update',
+          subject: 'Comment',
+          condition: { authorId: '${user.id}' },
+        },
+        {
+          action: 'delete',
+          subject: 'Comment',
+          condition: { authorId: '${user.id}' },
+        },
+        // Reaction permissions
+        {
+          action: 'create',
+          subject: 'Reaction',
+        },
+        {
+          action: 'delete',
+          subject: 'Reaction',
+          condition: { authorId: '${user.id}' },
+        },
+      ],
     });
   }
 }
