84ec7d71f9eb6a05cf3391a17e6315b11ea66bad
Manuel Martínez <104857693+manuelmtzv@users.noreply.github.com>
2024-08-14 21:25:45 +0000

    feat: add NestJS i18n module for internationalization support

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { ConfigModule } from '@nestjs/config';
-
+import { ForumModule } from './modules/forum/forum.module';
+import { AcceptLanguageResolver, I18nModule, QueryResolver } from 'nestjs-i18n';
 import mikroOrmConfig from '~/mikro-orm.config';
+import * as path from 'path';
+
 import {
   AuthModule,
   HealthModule,
@@ -13,7 +16,7 @@ import {
   ReactionModule,
   TopicModule,
 } from '@/modules';
-import { ForumModule } from './modules/forum/forum.module';
+
 @Module({
   imports: [
     MikroOrmModule.forRootAsync({
@@ -24,6 +27,17 @@ import { ForumModule } from './modules/forum/forum.module';
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
+    }),
+    I18nModule.forRoot({
+      fallbackLanguage: 'en',
+      loaderOptions: {
+        path: path.join(__dirname, '/i18n/'),
+        watch: true,
+      },
+      resolvers: [
+        { use: QueryResolver, options: ['lang'] },
+        AcceptLanguageResolver,
+      ],
     }),
     AuthModule,
     HealthModule,
