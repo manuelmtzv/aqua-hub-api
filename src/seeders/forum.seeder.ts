0eb98edb3b0de3d35f41a0f0ea4ad35ff7ed5dc7
Your Name <manuel.mtzv816@gmail.com>
2024-09-12 14:25:55 -0600

    feat: add English translations to forum and topic seeders

--- src/seeders/forum.seeder.ts
+++ src/seeders/forum.seeder.ts
@@ -4,40 +4,62 @@ import { Forum } from '@/entities';
 
 export class ForumSeeder extends Seeder {
   async run(em: EntityManager): Promise<void> {
-    em.create(Forum, {
-      translations: [
-        {
-          code: 'es',
-          title: 'Agua dulce',
-          description:
-            'Foro para discutir temas y compartir información acerca del cuidado y mantenimiendo de acuarios de agua dulce.',
-          enabled: true,
-        },
-      ],
-    });
-
-    em.create(Forum, {
-      translations: [
-        {
-          code: 'es',
-          title: 'Agua salada',
-          description:
-            'Foro para discutir temas y compartir información acerca del cuidado y mantenimiendo de acuarios de agua salada.',
-          enabled: true,
-        },
-      ],
-    });
-
-    em.create(Forum, {
-      translations: [
-        {
-          code: 'es',
-          title: 'General',
-          description:
-            'Foro para discutir temas, comentarios y consejos relacionados a la acuariofilia en general, tanto de agua salada como dulce.',
-          enabled: true,
-        },
-      ],
+    forums.forEach((forum) => {
+      em.create(Forum, forum);
     });
   }
 }
+
+const forums = [
+  {
+    translations: [
+      {
+        code: 'es',
+        title: 'Agua dulce',
+        description:
+          'Foro para discutir temas y compartir información acerca del cuidado y mantenimiento de acuarios de agua dulce.',
+        enabled: true,
+      },
+      {
+        code: 'en',
+        title: 'Freshwater',
+        description:
+          'Forum to discuss topics and share information about the care and maintenance of freshwater aquariums.',
+      },
+    ],
+  },
+  {
+    translations: [
+      {
+        code: 'es',
+        title: 'Agua salada',
+        description:
+          'Foro para discutir temas y compartir información acerca del cuidado y mantenimiento de acuarios de agua salada.',
+        enabled: true,
+      },
+      {
+        code: 'en',
+        title: 'Saltwater',
+        description:
+          'Forum to discuss topics and share information about the care and maintenance of saltwater aquariums.',
+      },
+    ],
+  },
+  {
+    translations: [
+      {
+        code: 'es',
+        title: 'General',
+        description:
+          'Foro para discutir temas, comentarios y consejos relacionados a la acuariofilia en general, tanto de agua salada como dulce.',
+        enabled: true,
+      },
+      {
+        code: 'en',
+        title: 'General',
+        description:
+          'Forum to discuss topics, comments, and advice related to aquaristics in general, including both saltwater and freshwater.',
+      },
+    ],
+  },
+];
--- src/seeders/topic.seeder.ts
+++ src/seeders/topic.seeder.ts
@@ -4,112 +4,160 @@ import { Topic } from '@/entities';
 
 export class TopicSeeder extends Seeder {
   async run(em: EntityManager): Promise<void> {
-    em.create(Topic, {
-      translations: [
-        {
-          code: 'es',
-          title: 'Alimentación',
-          description:
-            'Temas relacionados a la alimentación de los peces y animales acuáticos.',
-          enabled: true,
-        },
-      ],
-    });
-
-    em.create(Topic, {
-      translations: [
-        {
-          code: 'es',
-          title: 'Enfermedades',
-          description:
-            'Temas relacionados a las enfermedades de los peces y animales acuáticos.',
-          enabled: true,
-        },
-      ],
-    });
-
-    em.create(Topic, {
-      translations: [
-        {
-          code: 'es',
-          title: 'Equipamiento',
-          description:
-            'Temas relacionados al equipamiento necesario para mantener un acuario.',
-          enabled: true,
-        },
-      ],
-    });
-
-    em.create(Topic, {
-      translations: [
-        {
-          code: 'es',
-          title: 'Plantas',
-          description:
-            'Temas relacionados a las plantas acuáticas y su mantenimiento.',
-          enabled: true,
-        },
-      ],
-    });
-
-    em.create(Topic, {
-      translations: [
-        {
-          code: 'es',
-          title: 'Peces',
-          description:
-            'Temas relacionados a los peces y animales acuáticos en general.',
-          enabled: true,
-        },
-      ],
-    });
-
-    em.create(Topic, {
-      translations: [
-        {
-          code: 'es',
-          title: 'Reproducción',
-          description:
-            'Temas relacionados a la reproducción de los peces y animales acuáticos.',
-          enabled: true,
-        },
-      ],
-    });
-
-    em.create(Topic, {
-      translations: [
-        {
-          code: 'es',
-          title: 'Sustrato',
-          description:
-            'Temas relacionados al sustrato y su importancia en un acuario.',
-          enabled: true,
-        },
-      ],
-    });
-
-    em.create(Topic, {
-      translations: [
-        {
-          code: 'es',
-          title: 'Tratamiento de agua',
-          description:
-            'Temas relacionados al tratamiento del agua para mantener un acuario.',
-          enabled: true,
-        },
-      ],
-    });
-
-    em.create(Topic, {
-      translations: [
-        {
-          code: 'es',
-          title: 'Consejos generales',
-          description:
-            'Consejos generales para mantener un acuario en buen estado',
-          enabled: true,
-        },
-      ],
+    topics.forEach((topic) => {
+      em.create(Topic, topic);
     });
   }
 }
+
+const topics = [
+  {
+    translations: [
+      {
+        code: 'es',
+        title: 'Alimentación',
+        description:
+          'Temas relacionados a la alimentación de los peces y animales acuáticos.',
+        enabled: true,
+      },
+      {
+        code: 'en',
+        title: 'Feeding',
+        description: 'Topics related to feeding fish and aquatic animals.',
+      },
+    ],
+  },
+  {
+    translations: [
+      {
+        code: 'es',
+        title: 'Enfermedades',
+        description:
+          'Temas relacionados a las enfermedades de los peces y animales acuáticos.',
+        enabled: true,
+      },
+      {
+        code: 'en',
+        title: 'Diseases',
+        description: 'Topics related to diseases in fish and aquatic animals.',
+      },
+    ],
+  },
+  {
+    translations: [
+      {
+        code: 'es',
+        title: 'Equipamiento',
+        description:
+          'Temas relacionados al equipamiento necesario para mantener un acuario.',
+        enabled: true,
+      },
+      {
+        code: 'en',
+        title: 'Equipment',
+        description:
+          'Topics related to the equipment needed to maintain an aquarium.',
+      },
+    ],
+  },
+  {
+    translations: [
+      {
+        code: 'es',
+        title: 'Plantas',
+        description:
+          'Temas relacionados a las plantas acuáticas y su mantenimiento.',
+        enabled: true,
+      },
+      {
+        code: 'en',
+        title: 'Plants',
+        description: 'Topics related to aquatic plants and their maintenance.',
+      },
+    ],
+  },
+  {
+    translations: [
+      {
+        code: 'es',
+        title: 'Peces',
+        description:
+          'Temas relacionados a los peces y animales acuáticos en general.',
+        enabled: true,
+      },
+      {
+        code: 'en',
+        title: 'Fish',
+        description: 'Topics related to fish and aquatic animals in general.',
+      },
+    ],
+  },
+  {
+    translations: [
+      {
+        code: 'es',
+        title: 'Reproducción',
+        description:
+          'Temas relacionados a la reproducción de los peces y animales acuáticos.',
+        enabled: true,
+      },
+      {
+        code: 'en',
+        title: 'Reproduction',
+        description:
+          'Topics related to the reproduction of fish and aquatic animals.',
+      },
+    ],
+  },
+  {
+    translations: [
+      {
+        code: 'es',
+        title: 'Sustrato',
+        description:
+          'Temas relacionados al sustrato y su importancia en un acuario.',
+        enabled: true,
+      },
+      {
+        code: 'en',
+        title: 'Substrate',
+        description:
+          'Topics related to substrate and its importance in an aquarium.',
+      },
+    ],
+  },
+  {
+    translations: [
+      {
+        code: 'es',
+        title: 'Tratamiento de agua',
+        description:
+          'Temas relacionados al tratamiento del agua para mantener un acuario.',
+        enabled: true,
+      },
+      {
+        code: 'en',
+        title: 'Water Treatment',
+        description:
+          'Topics related to water treatment to maintain an aquarium.',
+      },
+    ],
+  },
+  {
+    translations: [
+      {
+        code: 'es',
+        title: 'Consejos generales',
+        description:
+          'Consejos generales para mantener un acuario en buen estado.',
+        enabled: true,
+      },
+      {
+        code: 'en',
+        title: 'General Advice',
+        description:
+          'General advice for keeping an aquarium in good condition.',
+      },
+    ],
+  },
+];
